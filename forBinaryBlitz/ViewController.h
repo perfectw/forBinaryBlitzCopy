d9c1e80e6b7c0d255a978e57809a230a605ac1c4
perfectw <roman.spirichkin@gmail.com>
2016-02-15 02:01:03 +0300

    Initial Commit

---
+++ forBinaryBlitz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  forBinaryBlitz
+//
+//  Created by Roman Spirichkin on 15/02/16.
+//  Copyright © 2016 rs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
